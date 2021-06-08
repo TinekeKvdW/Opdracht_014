bcd8337a98ea2cbe6db5f38d958a7abb8d9e35a8
TinekeKvdW <84176956+TinekeKvdW@users.noreply.github.com>
2021-06-08 10:30:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const maakPizzadeeg = function () {
+    console.log("250 gram bloem");
+    console.log("150ml lauwwarm water");
+    console.log("7 gram gist");
+    console.log("2 el olijfolie");
+    console.log("snufje zout");
+    console.log("Meng en kneed het deeg. Kneed, trek, knijp. Leef je uit.");
+    console.log("Laat het deeg een uur rijzen");
+    console.log("Na een uur leef je je nogmaals uit op het deeg en vervolgens rol je het plat met een deegroller");
+};
+
+maakPizzadeeg();
+maakPizzadeeg();
+
+const maakPizza = function () {
+    console.log("Maak tomatensaus met een pakje Heinz tomato Frito en van elk 1 tl oregano, basilicum en italiaanse kruiden");
+    console.log("Verdeel de zelfgemaakte tomatensaus over de pizzabodem");
+    console.log("Beleg de pizza met wat jij lekker vindt. Strooi als laatste geraspte kaas over de pizza");
+    console.log("Bak de pizza 10 min in een voorverwarmde oven op 250 graden");
+    console.log("Eet smakelijk");
+}
+
+maakPizza();
+maakPizza();
+maakPizza();
+
+// 3x pizza en 2x pizzabodem!!
